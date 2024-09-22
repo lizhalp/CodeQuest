98017f2d7b5e774758f142a6ae53bdc32457c307
lizhalp <119753562+lizhalp@users.noreply.github.com>
2024-09-22 19:56:14 -0400

    Erroneous inclusions

--- basic-starter-api-gateway-v1/src/lambda.js
+++
@@ -1,5 +0,0 @@
-require('source-map-support/register')
-const serverlessExpress = require('@codegenie/serverless-express')
-const app = require('./app')
-
-exports.handler = serverlessExpress({ app })
