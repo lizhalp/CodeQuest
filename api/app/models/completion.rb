1b2b030d53f2608d96dbe5262a08025ce0e613a6
lizhalp <119753562+lizhalp@users.noreply.github.com>
2024-10-18 15:32:37 -0400

    hi

--- api/app/models/completion.rb
+++
@@ -1,2 +0,0 @@
-class Completion < ApplicationRecord
-end
--- api/db/migrate/20241009202459_create_exercises.rb
+++
@@ -1,9 +0,0 @@
-class CreateExercises < ActiveRecord::Migration[8.0]
-  def change
-    create_table :exercises do |t|
-      t.text :options
-
-      t.timestamps
-    end
-  end
-end
