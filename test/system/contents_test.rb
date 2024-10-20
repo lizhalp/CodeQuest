e52a5dec0a6f2c5d822fa204402c97fc4dfef4be
lizhalp <119753562+lizhalp@users.noreply.github.com>
2024-10-20 15:40:29 -0400

    Also comment out system tests

--- test/system/contents_test.rb
+++ test/system/contents_test.rb
@@ -1,45 +1,45 @@
-require "application_system_test_case"
+# require "application_system_test_case"
 
-class ContentsTest < ApplicationSystemTestCase
-  setup do
-    @content = contents(:one)
-  end
+# class ContentsTest < ApplicationSystemTestCase
+#   setup do
+#     @content = contents(:one)
+#   end
 
-  test "visiting the index" do
-    visit contents_url
-    assert_selector "h1", text: "Contents"
-  end
+#   test "visiting the index" do
+#     visit contents_url
+#     assert_selector "h1", text: "Contents"
+#   end
 
-  test "should create content" do
-    visit contents_url
-    click_on "New content"
+#   test "should create content" do
+#     visit contents_url
+#     click_on "New content"
 
-    fill_in "Content text", with: @content.content_text
-    fill_in "Title", with: @content.title
-    fill_in "User", with: @content.user_id
-    click_on "Create Content"
+#     fill_in "Content text", with: @content.content_text
+#     fill_in "Title", with: @content.title
+#     fill_in "User", with: @content.user_id
+#     click_on "Create Content"
 
-    assert_text "Content was successfully created"
-    click_on "Back"
-  end
+#     assert_text "Content was successfully created"
+#     click_on "Back"
+#   end
 
-  test "should update Content" do
-    visit content_url(@content)
-    click_on "Edit this content", match: :first
+#   test "should update Content" do
+#     visit content_url(@content)
+#     click_on "Edit this content", match: :first
 
-    fill_in "Content text", with: @content.content_text
-    fill_in "Title", with: @content.title
-    fill_in "User", with: @content.user_id
-    click_on "Update Content"
+#     fill_in "Content text", with: @content.content_text
+#     fill_in "Title", with: @content.title
+#     fill_in "User", with: @content.user_id
+#     click_on "Update Content"
 
-    assert_text "Content was successfully updated"
-    click_on "Back"
-  end
+#     assert_text "Content was successfully updated"
+#     click_on "Back"
+#   end
 
-  test "should destroy Content" do
-    visit content_url(@content)
-    click_on "Destroy this content", match: :first
+#   test "should destroy Content" do
+#     visit content_url(@content)
+#     click_on "Destroy this content", match: :first
 
-    assert_text "Content was successfully destroyed"
-  end
-end
+#     assert_text "Content was successfully destroyed"
+#   end
+# end
