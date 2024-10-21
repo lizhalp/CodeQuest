7329fe13c638bafe532a8b4624299773bcd5f744
lizhalp <119753562+lizhalp@users.noreply.github.com>
2024-10-21 19:22:07 -0400

    Write more tests

--- test/models/completion_test.rb
+++ test/models/completion_test.rb
@@ -1,7 +1,25 @@
 require "test_helper"
 
 class CompletionTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:one)
+    @content = contents(:one)
+    @completion = Completion.new(user: @user, content: @content)
+  end
+
+  test "should require a user" do
+    @completion.user = nil
+    assert_not @completion.valid?
+  end
+
+  test "should require content" do
+    @completion.content = nil
+    assert_not @completion.valid?
+  end
+
+  test "should be unique for a given user and content" do
+    duplicate_completion = @completion.dup
+    @completion.save
+    assert_not duplicate_completion.valid?
+  end
 end
